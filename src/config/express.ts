2740a8a5c7c422615a9cf13028d1b4317e720080
diegodag201611 <dgaleano201611@gmail.com>
2023-04-01 15:43:15 -0500

    .env file inside a gitignore

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,10 @@
-import express, { Application } from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const configExpress = (app: Application): void => {
-  app.use(express.json())
-  app.use(morgan('dev'))
-  app.use(cors())
-}
-export default configExpress
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(cors());
+};
+export default configExpress;
