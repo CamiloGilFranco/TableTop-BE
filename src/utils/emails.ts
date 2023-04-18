695980f3b2e4556c22e138579d27d424e517b3bf
diegodag201611 <dgaleano201611@gmail.com>
2023-04-18 09:07:50 -0500

    add message to send email

--- src/utils/emails.ts
+++ src/utils/emails.ts
@@ -3,8 +3,8 @@ export const welcomeEmail = (user: any) => {
     from: "<admin@restaurant.com>",
     to: user.email,
     subject: "Welcome",
-    html: `<h1>Welcome ${user.name} thank you for registering to the top 27 restaurant</h1>`,
-    text: `Wellcome ${user.name} thank you for registering to the top 27 restaurant`,
+    html: `<h1>Welcome ${user.name} thank you for registering to the TableTop restaurant</h1>`,
+    text: `Wellcome ${user.name} thank you for registering to the TableTop restaurant`,
   };
   return email;
 };
