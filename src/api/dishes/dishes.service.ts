730c375456d921a2e1bf9ab684ddd69d56658ca7
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-04-21 23:43:44 -0500

    refactors create dish

--- src/api/dishes/dishes.service.ts
+++ src/api/dishes/dishes.service.ts
@@ -11,22 +11,33 @@ export const createDish = (input: any) => {
     title,
     description,
     price,
-    restaurantsId_restaurant,
-    dishes_categoriesId_dishes_category,
+    id_restaurant,
+    id_dishes_category,
     order_details,
   } = input;
+
+  console.log("Input:", input);
+  console.log("id_restaurant:", id_restaurant);
+
   return prisma.dishes.create({
     data: {
       title,
       description,
       price,
-      restaurantsId_restaurant,
-      dishes_categoriesId_dishes_category,
       order_details,
+      restaurants: {
+        connect: {
+          id_restaurant: id_restaurant,
+        },
+      },
+      dishes_categories: {
+        connect: {
+          id_dishes_category, // Use the provided id_dishes_category
+        },
+      },
     },
   });
 };
-
 export const getDishById = (id: string) => {
   return prisma.dishes.findUnique({
     where: {
